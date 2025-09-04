90761c833a0b122876cebf4f133fde4ef94b5ed8
pprudnikova10-stack <pprudnikova10@gmail.com>
2025-09-04 17:18:13 +0300

    Add files via upload

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,13 +4,14 @@
     {
         public static void Main()
         {
-            //White white = new White();
-            //Console.WriteLine(white.Task1(1.2));
-            //Console.WriteLine(white.Task2(2));
-            //Console.WriteLine(white.Task3(2.5, 1.89));
+            Blue blue = new Blue();
+            Console.WriteLine(blue.Task1(1.2));
+            Console.WriteLine(blue.Task2(2));
+            Console.WriteLine(blue.Task3(2, 1));
             //Console.WriteLine(white.Task4(-2.5, 1.89));
             //Console.WriteLine(white.Task5(0.5));
             //Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            Console.WriteLine(blue.Task7(2));
         }
     }
 }
