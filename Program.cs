e0e19166d4cca0ea071c395a26d5f350c910b968
pprudnikova10-stack <pprudnikova10@gmail.com>
2025-09-04 17:10:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Blue blue = new Blue();
+            Console.WriteLine(blue.Task1(1.2));
+            Console.WriteLine(blue.Task2(2));
+            Console.WriteLine(blue.Task3(2, 1));
+            //Console.WriteLine(white.Task4(-2.5, 1.89));
+            //Console.WriteLine(white.Task5(0.5));
+            //Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            Console.WriteLine(blue.Task7(2));
+        }
+    }
+}
